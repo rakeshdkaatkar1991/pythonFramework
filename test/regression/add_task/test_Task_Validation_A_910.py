5f056e490495b05d8ca0885f35c630fa32e790e4
Ramya Tn (DOP - Business Platforms) <ramyalishanvi@gmail.com>
2019-11-29 12:59:55 +0530

    added testValidation script, Create driver file

--- test/regression/add_task/test_Task_Validation_A_910.py
+++ test/regression/add_task/test_Task_Validation_A_910.py
@@ -5,6 +5,7 @@ from lib.ui.home_page import HomePage
 import json
 import unittest
 import time
+import pdb
 
 class task_Validation_A_910(unittest.TestCase):
     def setUp(self):
@@ -31,13 +32,18 @@ class task_Validation_A_910(unittest.TestCase):
     def test_add_Task_TC001(self):
         #------------------Fetch testdata required for add task script from json----------------------
         data=json.load(open('./test/regression/add_task/A_910.json'))
+        time.sleep(3)
         #---------click on task tab-----------------------
         self.task_obj.get_Tasks_tab().click()
+
+        #Test script to add Customer------------------------------------
         #----------Click on add new button---------------
         self.task_obj.get_AddNew_button().click()
         time.sleep(5)
         #-----------click on New Customer Option------------
-        self.task_obj.get_New_Customer_option().click()
+        #self.task_obj.get_ddl_from_AddNew('+ New Customer')
+        self.task_obj.get_options_from_dropdown('+ New Customer',self.task_obj.get_Add_New_ddl(),self.task_obj.get_New_Customer_option('+ New Customer'))
+        #self.task_obj.get_New_Customer_option().click()
         #----------wait for Create New Customer Page to load---------------
         time.sleep(10)
         #self.task_obj.wait_for_Create_New_Customer_Page_load()
@@ -47,23 +53,45 @@ class task_Validation_A_910(unittest.TestCase):
         self.task_obj.get_Customer_Description_txtbox().send_keys(data["TC001"]["CustomerDescription"])
         #---------------Click on Select customer dropdown------------------
         self.task_obj.get_Customer_drpdown().click()
-        #--------------Get Customer drop down list and save it to some variable---------------------
-        Cust_list=self.task_obj.get_Cust_dropdown_list()
-        #-----------Get the required value from json file
-        required_value=data["TC001"]["CustNamefromDrpDown"]
-        #------------Loop through the list and click on required value
-        size=len(Cust_list)
-        for i in range(0,size):
-            drpdownValue=Cust_list[i]
-            text=drpdownValue.text
-            if text==required_value:
-                Cust_list[i].click()
-                break
-        else:
-            print('Required value not found in dropdown')
-
+        #--------------Get Customer drop down list and Split it then save it to some variable---------------------
+        self.task_obj.get_options_from_dropdown('Galaxy Corporation',self.task_obj.get_Cust_dropdown_list(),self.task_obj.get_Required_Project_option('Galaxy Corporation'))
+        #-------------Use scroll option of js----------------------
+        js_command='window.scroll(0,5000)'
+        self.driver.execute_script(js_command)
         #--------------Click on Create Customer button--------------------
-        self.task_obj.get_Create_Customer_btn()
+        self.task_obj.get_Create_Customer_btn().click()
+        time.sleep(10)
+
+        #------------Create Project script----------------------------
+
+        # ----------Click on add new button---------------
+        self.task_obj.get_AddNew_button().click()
+        time.sleep(5)
+        # -----------click on New Project Option------------
+        self.task_obj.get_options_from_dropdown('+ New Project',self.task_obj.get_Add_New_ddl(),self.task_obj.get_New_Project_option('+ New Project'))
+        time.sleep(3)
+        #-----------------Enter Project Name----------------------
+        self.task_obj.get_Project_Name_txtbox().send_keys(data["TC001"]["ProjectName"])
+        #-----------------Enter project description----------------------
+        self.task_obj.get_Project_description().send_keys(data["TC001"]["ProjectDescription"])
+        #-----------------Enter value to Task field------------------------
+        self.task_obj.get_Task_txtbox().send_keys(data["TC001"]["TaskName"])
+        #--------------Enter Task description-------------------------
+        self.task_obj.get_Task_DesriptionIcon().click()
+        self.task_obj.get_Task_DesriptionArea().send_keys(data["TC001"]["TaskDescription"])
+        #------------------- Click on Save button ----------------------
+        self.task_obj.get_SaveBtn().click()
+        #--------------Enter Estimate-----------------
+        self.task_obj.get_Estimate().send_keys(data["TC001"]["Estimate"])
+        #-------------Click on Create Project button-----------------
+        js_command='window.scroll(0,5000)'
+        self.driver.execute_script(js_command)
+        self.task_obj.get_CreateProjectButton().click()
+        time.sleep(5)
+
+
+
+
 
 
 
